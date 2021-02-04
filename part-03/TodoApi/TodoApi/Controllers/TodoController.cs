704d6e8523404db817c2facd0773a52e08616592
Sebastian Jensen <apps@tsjdev-apps.de>
2021-02-04 21:28:20 +0100

    Add part 3 of Todo API.

---
+++ part-03/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using TodoApi.Services;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoFileService _todoFileService;
+
+        public TodoController(ITodoFileService todoFileService)
+        {
+            _todoFileService = todoFileService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<TodoItem>>> GetAsync()
+        {
+            var todoItems = await _todoFileService.LoadDataAsync();
+            return Ok(todoItems);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<TodoItem>> GetByIdAsync(string id)
+        {
+            var todoItem = await GetTodoItemAsync(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            return Ok(todoItem);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<TodoItem>> AddAsync(TodoItemRequest todoItemRequest)
+        {
+            if (string.IsNullOrEmpty(todoItemRequest.Content))
+                return BadRequest("No content provided");
+
+            var todoItem = new TodoItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                Content = todoItemRequest.Content,
+                IsDone = todoItemRequest.IsDone,
+                TimeStamp = DateTime.Now
+            };
+
+            var todoItems = await _todoFileService.LoadDataAsync();
+            todoItems.Add(todoItem);
+            await _todoFileService.SaveDataAsync(todoItems);
+
+            return Ok(todoItem);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteAsync(string id)
+        {
+            var todoItems = await _todoFileService.LoadDataAsync();
+            var todoItem = todoItems.FirstOrDefault(t => t.Id == id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            todoItems.Remove(todoItem);
+            await _todoFileService.SaveDataAsync(todoItems);
+
+            return Ok();
+        }
+
+        [HttpPut("status/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<TodoItem>> UpdateAsync(string id)
+        {
+            var todoItems = await _todoFileService.LoadDataAsync();
+            var todoItem = todoItems.FirstOrDefault(t => t.Id == id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            todoItem.IsDone = !todoItem.IsDone;
+            todoItem.TimeStamp = DateTime.Now;
+
+            await _todoFileService.SaveDataAsync(todoItems);
+
+            return Ok(todoItem);
+        }
+
+        private async Task<TodoItem> GetTodoItemAsync(string id)
+        {
+            var todoItems = await _todoFileService.LoadDataAsync();
+            return todoItems.FirstOrDefault(t => t.Id == id);
+        }
+    }
+}
---
+++ part-03/TodoApi/TodoApi/Startup.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TodoApi.Services;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSingleton<ITodoFileService, TodoFileService>();
+
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "ToDo API";
+                    document.Info.Description = "A simple ASP.NET Core ToDo API";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Thomas Sebastian Jensen",
+                        Email = "apps@tsjdev-apps.de",
+                        Url = "https://www.tsjdev-apps.de"
+                    };
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
